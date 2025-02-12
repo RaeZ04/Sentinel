432d77f617379d56d5d8887e5bd01c50097ef15d
Pelukss <sorrobloko@gmail.com>
2025-02-12 02:30:17 +0100

    organizacion de directorios

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.pruebaexe {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens org.example.pruebaexe to javafx.fxml;
-    exports org.example.pruebaexe;
-}
--- src/main/java/org/example/pruebaexe/App.java
+++
@@ -1,7 +0,0 @@
-package org.example.pruebaexe;
-
-public class App {
-    public static void main(String[] args) {
-        AppInitializer.main(args);
-    }
-}
